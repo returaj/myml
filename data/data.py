02dcde034b13c364b0fff4fd847208c8babfb0b3
returaj <returaj123burnwal@gmail.com>
2021-02-27 02:00:56 +0530

    add knn

--- data/data.py
+++ data/data.py
@@ -7,6 +7,12 @@ from matplotlib import pyplot as plt
 
 
 class Data:
+    def to_label(self, D):
+        np.random.shuffle(D)
+        X, Y = [], []
+        for x, y in D:
+            X.append(x); Y.append(y)
+        return np.array(X), np.array(Y)
 
     def circle(self, n1, n2):
         def circle(r):
@@ -15,62 +21,69 @@ class Data:
             y = np.sqrt(r*r - x*x) * sign
             return x, y
 
-        X1, X2 = [], []
+        D = []
         r1, r2 = 1, 3
+        l1, l2 = 0, 1
         for i in range(n1):
             x, y = circle(r1)
-            X1.append((x, y))
+            D.append(((x, y), l1))
         for i in range(n2):
             x, y = circle(r2)
-            X2.append((x, y))
-        return X1, X2
+            D.append(((x, y), l2))
+        return self.to_label(D)
 
     def xor(self):
-        X1 = [(0, 0), (1, 1)]
-        X2 = [(0, 1), (1, 0)]
-        return X1, X2
+        l1, l2 = 0, 1
+        X1 = [((0, 0), l1), ((1, 1), l1)]
+        X2 = [((0, 1), l2), ((1, 0), l2)]
+       
+        D = X1 + X2
+        return self.to_label(D)
+
 
     def ls_balanced(self, n1, n2):
-        X1, X2 = [], []
+        l1, l2 = 0, 1
+        D = []
         x1, y1 = (5, 10), (-5, 5)
         for i in range(n1):
             x, y = np.random.uniform(*x1), np.random.uniform(*y1)
-            X1.append((x, y))
+            D.append(((x, y), l1))
         x2, y2 = (-2, 3), (-5, 5)
         for i in range(n2):
             x, y = np.random.uniform(*x2), np.random.uniform(*y2)
-            X2.append((x, y))
-        return X1, X2
+            D.append(((x, y), l2))
+        return self.to_label(D)
 
     def ls_unbalanced(self, n1, n2):
-        X1, X2 = [], []
+        l1, l2 = 0, 1
+        D = []
         x1, y1 = (7, 10), (-5, 5)
         for i in range(n1):
             x, y = np.random.uniform(*x1), np.random.uniform(*y1)
-            X1.append((x, y))
+            D.append(((x, y), l1))
         extreme_n2 = 3
         ex2, ey2 = (5, 6), (-3, 3)
         for i in range(extreme_n2):
             x, y = np.random.uniform(*ex2), np.random.uniform(*ey2)
-            X2.append((x, y))
+            D.append(((x, y), l2))
         clusture_n2 = n2 - extreme_n2
         cx2, cy2 = (-3, 1), (-5, 5)
         for i in range(clusture_n2):
             x, y = np.random.uniform(*cx2), np.random.uniform(*cy2)
-            X2.append((x, y))
-        return X1, X2
+            D.append(((x, y), l2))
+        return self.to_label(D)
 
 
-def visualize(X1, X2, title, figname):
+def visualize(X, Y, title, figname):
     x1, y1 = [], []
-    for x, y in X1:
-        x1.append(x)
-        y1.append(y)
-
     x2, y2 = [], []
-    for x, y in X2:
-        x2.append(x)
-        y2.append(y)
+    for x, y in zip(X, Y):
+        if y == 0:
+            x1.append(x[0])
+            y1.append(x[1])
+        else:
+            x2.append(x[0])
+            y2.append(x[1])
 
     fig = plt.figure()
     ax = fig.add_subplot()
---
+++ knn/k_nearest_neighbour.py
@@ -0,0 +1,105 @@
+#! /usr/bin/env python3
+
+import numpy as np
+import matplotlib
+matplotlib.use('Agg')
+from matplotlib import pyplot as plt
+from collections import defaultdict
+from data.data import Data
+from model.base_model import BaseModel
+
+
+class KNN(BaseModel):
+    def __init__(self, k, dist):
+        self.k = k
+        self.dist = dist
+
+    def train(self, X, Y):
+        self.X = X
+        self.Y = Y
+
+    @staticmethod
+    def vote(d, k):
+        y = defaultdict(lambda : 0)
+        max_v = 0
+        for i in range(min(k, len(d))):
+            _, label = d[i]
+            y[label] += 1
+            max_v = max(max_v, y[label])
+        labels_set = [k for k, v in y.items() if max_v == v]
+        if len(labels_set) > 1:
+            return self.vote(d, k-2)
+        return labels_set[0]
+
+    def predict(self, x):
+        d = []
+        for dx, dy in zip(self.X, self.Y):
+            d.append((self.dist(x, dx), dy))
+        d.sort(key=lambda x: x[0])
+        return self.vote(d, self.k)
+
+
+def distance_func(p):
+    def dist(x1, x2):
+        diff = np.abs(x1-x2)
+        max_v = np.max(diff)
+        if p > 6 or (max_v > 50 and p > 3):
+            return max_v
+        return np.power(np.sum(np.power(diff, p)), 1/p)
+    return dist
+
+
+def cosine_fun():
+    return lambda x1, x2: np.dot(x1, x2) / (np.linalg.norm(x1) * np.linalg.norm(x2))
+
+
+def test_accuracy(knn, X, Y):
+    acc = 0
+    for x, y in zip(X, Y):
+        acc += 1 if y == knn.predict(np.array(x)) else 0
+    return acc / len(X)
+
+
+def visualize(knn, x_range, y_range, figname):
+    x = np.arange(*x_range, 0.1)
+    y = np.arange(*y_range, 0.1)
+    xx, yy = np.meshgrid(x, y)
+    fx, fy = xx.flatten(), yy.flatten()
+    Xhat = np.column_stack((fx, fy))
+    Yhat = []
+    for x in Xhat:
+        Yhat.append(knn.predict(x))
+    zz = np.array(Yhat).reshape(xx.shape)
+
+    plt.contourf(xx, yy, zz, cmap='Paired')
+    plt.savefig(figname)
+
+
+def experiment():
+    k = 5
+    p = 2
+
+    data = Data()
+
+    X, Y = data.circle(n1=200, n2=200)
+
+    knn = KNN(k, distance_func(p))
+    knn.train(X, Y)
+
+    testX, testY = data.circle(n1=40, n2=40)
+    print(test_accuracy(knn, testX, testY))
+
+    x, y = [], []
+    for a, b in X:
+        x.append(a); y.append(b)
+    min_x, max_x = np.min(x), np.max(x)
+    min_y, max_y = np.min(y), np.max(y)
+    visualize(knn, (min_x, max_x), (min_y, max_y), "knn/circle.png")
+
+
+if __name__ == '__main__':
+    experiment()
+    
+
+
+
